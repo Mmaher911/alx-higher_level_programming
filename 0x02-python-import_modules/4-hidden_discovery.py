4d9ba760214c9267da7758380679f72b822e9858
Mmaher911 <mohamed.saidco@gmail.com>
2024-01-04 14:29:39 -0800

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.4
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    import hidden_4
+
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
