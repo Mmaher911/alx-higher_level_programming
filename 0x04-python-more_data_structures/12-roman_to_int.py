5f4caeae1ee7abe6432148f4f42bb8399d07485e
Mmaher911 <mohamed.saidco@gmail.com>
2024-01-10 12:08:24 -0800

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
 	return 0
-    t = 0
-    n = 0
-    num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    total = 0
+    num = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for r in reversed(roman_string):
-        n = num[r]
-        t += n if t < n * 5 else -n
-    return t
+        num = digits[r]
+        total += num if total < num * 5 else -num
+    return total
