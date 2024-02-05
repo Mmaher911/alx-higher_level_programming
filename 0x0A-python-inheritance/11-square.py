ad13f50d05c2ae0eb0347dc520ae95e2ef40c557
Mmaher911 <mohamed.saidco@gmail.com>
2024-02-05 09:02:59 -0800

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
+""" 0x0A. Python - Inheritance, task 8 """
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """Inherits from Rectangle and thus from BaseGeometry, for use with
+    rectangular constructs of equal sides.
+
+    Args:
+        size (int): length of side of square
+
+    Attributes:
+        __size (int): length of side of square
+
+    """
     def __init__(self, size):
-        """ size init"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Returns square area as size * size, invoking Rectangle.area().
+
+        Attributes:
+            __size (int): length of side of square
+
+        Returns:
+            __size ** 2
+
+        """
+        return self.__size ** 2
 
     def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        """ Provides printable representation of Square object.
+
+        """
+        return '[Square] {}/{}'.format(self.__size, self.__size)
